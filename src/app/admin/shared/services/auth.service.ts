ebd93549bc998cffca93d986f9a84a23c013e486
otus-dn-core <otus@dn-core.ru>
2021-08-07 22:39:16 +0300

    Token, before Guard

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -2,28 +2,43 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../../shared/interfaces';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  get token(): string {
-    return ''
+  get token(): string | null {
+    return localStorage.getItem('nest-token');
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user)
+    const userobj = {user: user}
+    //console.log(userobj);
+     return this.http.post('http://localhost:3000/users/login', userobj)
+    //return this.http.post('http://localhost:3000/users', userobj)
+
+    // return this.http.post('http://localhost:3000/users/login', user)
+    // return this.http.post('http://localhost:3000/users', user)
+    .pipe(
+      tap(this.setToken)
+    )
   }
 
   logout() {
-
+    this.setToken(null)
   }
 
   isAuthenticated(): boolean {
     return !!this.token
   }
 
-  private setToken() {
-
+  private setToken(response: any | null) {
+    if (response) {
+      console.log(response.user.token);
+    localStorage.setItem('nest-token', response.user.token);
+    } else {
+      localStorage.clear();
+    }
   }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -3,3 +3,7 @@ export interface User {
   email: string
   password: string
 }
+
+export interface Userobj {
+  user: object
+}
